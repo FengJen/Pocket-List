c7dc857994c59f5408ffc65597a750e08c55c817
fengjen <frd92517@gmail.com>
2017-03-27 17:47:06 +0800

    store url with title

--- 口袋名單/口袋名單/Step3ViewController.swift
+++ 口袋名單/口袋名單/Step3ViewController.swift
@@ -2,12 +2,20 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class Step3ViewController: UIViewController, Step2delegate {
+class Step3ViewController: UIViewController {
+    
+    @IBOutlet weak var temperaryTitle: UITextField!
     @IBOutlet weak var website: UITextField!
     let ref = FIRDatabase.database().reference().child("user")
     
     @IBAction func doneFillingInfo(_ sender: Any) {
-        if title == "" {
+        
+        if temperaryTitle.text == "" {
+            let allert = UIAlertController(title: "您還未輸入標題", message: "請在輸入新增項目的標題", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            allert.addAction(action)
+            self.present(allert, animated: true, completion: nil)
+        } else if website.text == "" {
             let allert = UIAlertController(title: "您還未輸入網址", message: "請複製貼上想儲存的網址（例如google map網址）", preferredStyle: .alert)
             let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             allert.addAction(action)
@@ -20,23 +28,23 @@ class Step3ViewController: UIViewController, Step2delegate {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        Step2ViewController.shared.delegate = self
+        //Step2ViewController.shared.delegate = self
     }
     
-    func passTitle(title: String) {
-        if title == "" {
-            let allert = UIAlertController(title: "您還未輸入標題", message: "請在step 2輸入新增項目的標題", preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            allert.addAction(action)
-            self.present(allert, animated: true, completion: nil)
-        } else {
-        ref.childByAutoId().setValue(["title": title])
-        }
-    }
+//    func passTitle(title: String) {
+//        if title == "" {
+//            let allert = UIAlertController(title: "您還未輸入標題", message: "請在step 2輸入新增項目的標題", preferredStyle: .alert)
+//            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//            allert.addAction(action)
+//            self.present(allert, animated: true, completion: nil)
+//        } else {
+//        ref.childByAutoId().setValue(["title": title])
+//        }
+//    }
     func uploadData() {
-        if let uid = constants.uid, let url = website.text {
+        if let uid = constants.uid, let url = website.text, let title = temperaryTitle.text {
                 let cell = ref.child(uid).childByAutoId()
-                cell.setValue(["url": url])
+            cell.setValue(["title": title, "url": url])
         }
     }
 }
