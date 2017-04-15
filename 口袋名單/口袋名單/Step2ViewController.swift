aa5826c67bca5e151d3b6722254822e135a2477d
fengjen <frd92517@gmail.com>
2017-04-15 16:22:33 +0800

    fix layout

--- 口袋名單/口袋名單/Step2ViewController.swift
+++ 口袋名單/口袋名單/Step2ViewController.swift
@@ -1,26 +1,42 @@
-//
-//  Step2ViewController.swift
-//  口袋名單
-//
-//  Created by 謝豐任 on 2017/3/22.
-//  Copyright © 2017年 appworks. All rights reserved.
-//
-
 import UIKit
 import Firebase
 import FirebaseDatabase
 
 class Step2ViewController: UIViewController {
     
-    //static let shared = Step2ViewController()
-  //  weak var delegate: Step2delegate?
+    let step1VC = Step1ViewController()
+    let ref = FIRDatabase.database().reference().child("pocketList")
+        //UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Step1ViewController")
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
 
+    @IBAction func doneButton(_ sender: Any) {
+        
+        if step1VC.temperaryTitle.text == "" {
+            let allert = UIAlertController(title: "您還未輸入標題", message: "請在輸入新增項目的標題", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            allert.addAction(action)
+            self.present(allert, animated: true, completion: nil)
+        } else if step1VC.website.text == "" {
+            let allert = UIAlertController(title: "您還未輸入網址", message: "請複製貼上想儲存的網址（例如google map網址）", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            allert.addAction(action)
+            self.present(allert, animated: true, completion: nil)
+        } else {
+            uploadData()
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ParentViewController")
+            navigationController?.pushViewController(vc, animated: true)
+        }
+
+    }
+    func uploadData() {
+        if let uid = Constants.uid, let url = step1VC.website.text, let title = step1VC.temperaryTitle.text {
+            let cell = ref.child(uid).childByAutoId()
+            cell.setValue(["title": title, "url": url, "order": CellDataManager.shared.cellArray.count])
+        }
+    }
 }
