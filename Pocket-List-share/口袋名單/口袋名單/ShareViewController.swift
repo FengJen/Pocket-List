2ce7039da0c5cf4f1023c7fa08b9e8e9b901f0d9
fengjen <frd92517@gmail.com>
2017-04-24 20:54:22 +0800

    share done

--- Pocket-List-share/口袋名單/口袋名單/ShareViewController.swift
+++ Pocket-List-share/口袋名單/口袋名單/ShareViewController.swift
@@ -21,38 +21,60 @@ class ShareViewController: UIViewController {
     
     weak var delegate: DidReceivePackage?
     
+    let uid = FIRAuth.auth()?.currentUser?.uid
+    var senderEmail = ""
     @IBAction func receive(_ sender: Any) {
-        
-        guard let text = sharingKey.text else { return }
-        FIRDatabase.database().reference().child("package").queryOrdered(byChild: "packageID").queryEqual(toValue: text).observeSingleEvent(of: .value, with: { (snapshot) in
-            guard let newCells = snapshot.value as? [String: Any] else { return }
-            for newCell in newCells {
-                let uid = FIRAuth.auth()?.currentUser?.uid
-                guard let value = newCell.value as? [String: AnyObject] else { continue }
+        FIRDatabase.database().reference().child("userEmail").child(uid!).observeSingleEvent(of: .value, with: { (emailSnapshot) in
+            guard let email = emailSnapshot.value as? [String: Any] else { return }
+            guard let myEmail = email["email"] as? String else { return }
+            print(myEmail)
+            //guard let text = sharingKey.text else { return }
+            FIRDatabase.database().reference().child("package").queryOrdered(byChild: "receiverEmail").queryEqual(toValue: myEmail).observeSingleEvent(of: .value, with: { (snapshot) in
                 
-                guard let cellList = value["cellList"] as? Array<Dictionary<String, Any>> else { continue }
-                
-                FIRDatabase.database().reference().child("pocketList").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
-                    if snapshot.exists() {
+                for child in snapshot.children {
+                    
+                    guard let item = child as? FIRDataSnapshot else { return }
+                    
+                    guard let value = item.value as? [String: Any] else { return }
+                    guard let email = value["senderEmail"] as? String else { return }
+                    self.senderEmail = email
+                }
+                print(self.senderEmail)
+                let alertController = UIAlertController(title: "確認傳送來源", message: "\(self.senderEmail)想要和你分享他的口袋名單", preferredStyle: .alert)
+                guard let newCells = snapshot.value as? [String: Any] else { return }
+                let accept = UIAlertAction(title: "同意", style: .default) { (action) in
+                    for newCell in newCells {
+                        let uid = FIRAuth.auth()?.currentUser?.uid
+                        guard let value = newCell.value as? [String: AnyObject] else { continue }
                         
-                        for cell in cellList {
-                            FIRDatabase.database().reference().child("pocketList").child(uid!).updateChildValues(cell)
-                            
-                        }
+                        guard let cellList = value["cellList"] as? Array<Dictionary<String, Any>> else { continue }
                         
-                    } else {
-                        
-                        for cell in cellList {
-                            FIRDatabase.database().reference().child("pocketList").child(uid!).updateChildValues(cell)
-                            
-                        }
-                        
+                        FIRDatabase.database().reference().child("pocketList").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                            if snapshot.exists() {
+                                
+                                for cell in cellList {
+                                    FIRDatabase.database().reference().child("pocketList").child(uid!).updateChildValues(cell)
+                                    
+                                }
+                                
+                            } else {
+                                
+                                for cell in cellList {
+                                    FIRDatabase.database().reference().child("pocketList").child(uid!).updateChildValues(cell)
+                                }
+                            }
+                            self.delegate?.didReceive(shareVC: self, uploadSuccess: true)
+                        })
                     }
-                    self.delegate?.didReceive(shareVC: self, uploadSuccess: true)
-                })
-            }
+                }
+                let decline = UIAlertAction(title: "拒絕", style: .destructive, handler: nil)
+                //todo delete package in 5mins?
+                alertController.addAction(accept)
+                alertController.addAction(decline)
+                self.present(alertController, animated: true, completion: nil)
+   
+            })
         })
-        
     }
     
     func alert() {
